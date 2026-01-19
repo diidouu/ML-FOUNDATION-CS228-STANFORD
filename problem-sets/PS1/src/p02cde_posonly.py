f3841377dfad20dceb88ff43ae8e9c6c8f14694c
Ismaïl EL GOTE <160734505+diidouu@users.noreply.github.com>
2026-01-19 13:43:47 +0900

    exercise 2 PS 01

--- problem-sets/PS1/src/p02cde_posonly.py
+++ problem-sets/PS1/src/p02cde_posonly.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 import numpy as np
 import util
 
@@ -26,10 +27,42 @@ def main(train_path, valid_path, test_path, pred_path):
     pred_path_e = pred_path.replace(WILDCARD, 'e')
 
     # *** START CODE HERE ***
-    # Part (c): Train and test on true labels
-    # Make sure to save outputs to pred_path_c
-    # Part (d): Train on y-labels and test on true labels
-    # Make sure to save outputs to pred_path_d
-    # Part (e): Apply correction factor using validation set and test on true labels
-    # Plot and use np.savetxt to save outputs to pred_path_e
+    # Load datasets
+    x_train, t_train = util.load_dataset(train_path, label_col = 't', add_intercept = True)
+    x_valid, t_valid = util.load_dataset(valid_path, label_col = 't', add_intercept = True)
+    x_test, t_test = util.load_dataset(test_path, label_col = 't', add_intercept = True)
+    
+    # using t-labels for training
+    model_t = LogisticRegression()
+    model_t.fit(x_train, t_train)
+    t_pred_c = model_t.predict(x_test)
+    np.savetxt(pred_path_c, t_pred_c)
+    
+    # using y-labels for training
+    x_train, y_train = util.load_dataset(train_path, label_col = 'y', add_intercept = True)
+    x_valid, y_valid = util.load_dataset(valid_path, label_col = 'y', add_intercept = True)
+    x_test, y_test = util.load_dataset(test_path, label_col = 'y', add_intercept = True)
+    
+    model_y = LogisticRegression()
+    model_y.fit(x_train, y_train)
+    y_pred_d = model_y.predict(x_test)
+    np.savetxt(pred_path_d, y_pred_d)
+    
+    # using y-labels with correction factor alpha
+    x_train, y_train = util.load_dataset(train_path, label_col = 'y', add_intercept = True)
+    x_valid, y_valid = util.load_dataset(valid_path, label_col = 'y', add_intercept = True)
+    x_test, y_test = util.load_dataset(test_path, label_col = 'y', add_intercept = True)
+    
+    model_alpha_y = LogisticRegression()
+    model_alpha_y.fit(x_train, y_train)
+    y_pred_e = model_alpha_y.predict(x_valid)
+    alpha = np.mean(y_pred_e[y_valid == 1])
+    y_pred_e = model_alpha_y.predict(x_test) / alpha
+    np.savetxt(pred_path_e, y_pred_e)
+    
+    
+
+    
+   
+            
     # *** END CODER HERE
